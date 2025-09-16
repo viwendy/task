d0b8efa7350523768337cb7a62fc3a244fec097b
viwendy <sheldan@163.com>
2025-09-16 22:24:50 +0800

    task1 add

---
+++ task1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	intArr := []int{2, 2, 1}
+	num := appearOnlyOnceNum(intArr)
+	fmt.Println(num)
+}
+
+func appearOnlyOnceNum(nums []int) int {
+	var intMap = make(map[int]int)
+	for _, num := range nums {
+		intMap[num] += 1
+	}
+	for k, v := range intMap {
+		if v == 1 {
+			return k
+		}
+	}
+	return -1
+}
