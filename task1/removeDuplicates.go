f1c8ef9822a6e8f6a9fe3aaaac93f0c88b9f3531
viwendy <sheldan@163.com>
2025-09-17 00:38:39 +0800

    fix

--- task1/removeDuplicates.go
+++ task1/removeDuplicates.go
@@ -3,19 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	intArr := []int{1, 1, 2}
+	intArr := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
 	count, intData := removeDuplicates(intArr)
 	fmt.Println(count, ", nums =", intData)
 }
 
 func removeDuplicates(nums []int) (int, []int) {
-	newIntArr := []int{}
-	intMap := make(map[int]int)
-	for _, num := range nums {
-		intMap[num] += 1
+	if len(nums) == 0 {
+		return 0, nums
 	}
-	for v := range intMap {
-		newIntArr = append(newIntArr, v)
+	slow := 0
+	for fast := 1; fast < len(nums); fast++ {
+		// 当发现不同的元素时
+		if nums[fast] != nums[slow] {
+			// 将不重复元素放到slow+1位置
+			slow++
+			nums[slow] = nums[fast]
+			//fmt.Println(nums)
+			//break
+		}
 	}
-	return len(intMap), newIntArr
+	return slow + 1, nums[:slow+1]
 }
