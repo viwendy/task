3fd9740ffebfd1a94ea9c623928e286875a82a83
viwendy <sheldan@163.com>
2025-09-17 22:30:07 +0800

    fix

--- task1/twoSum.go
+++ task1/twoSum.go
@@ -1,7 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 	intArr := []int{2, 7, 11, 15}
 	target := 9
-	twoSum(intArr, target)
+	arr := twoSum(intArr, target)
+	fmt.Println(arr)
+}
+
+func twoSum(intArr []int, target int) []int {
+	for k, v := range intArr {
+		for i := k + 1; i < len(intArr); i++ {
+			if v+intArr[i] == target {
+				return []int{k, i}
+			}
+		}
+	}
+	return nil
 }
